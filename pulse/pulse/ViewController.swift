f4b4469a026323f57565826ddd3eab307f6d0d85
kamenibzv <kamenibzv@users.noreply.github.com>
2019-06-20 19:26:18 -0700

    Base project (#2)
    * Adding a message to test pushing from my local repo
    
    * Revert "Adding a message to test pushing from my local repo"
    
    This reverts commit 9496d77c0cde0493f948a775bb4afe5ddd1eb1db.
    
    * Creating the initial project

---
+++ pulse/pulse/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  pulse
+//
+//  Created by Kameni Ngahdeu on 6/17/19.
+//  Copyright © 2019 bdt. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
