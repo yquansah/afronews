048a76cfdd862ddc7b076da6670e875775d0207a
emilshouse <emilshouse@gmail.com>
2019-07-23 19:44:53 -0700

    datafiles created

---
+++ pulse/pulse/ArticleStore.swift
@@ -0,0 +1,15 @@
+//
+//  ArticleStore.swift
+//  pulse
+//
+//  Created by Adnan Abdulai on 7/14/19.
+//  Copyright © 2019 bdt. All rights reserved.
+//
+
+import UIKit
+
+class ArticleStore: NSObject, Codable {
+
+    var allArticles = [Article]()
+    
+}
---
+++ pulse/pulse/Saved.swift
@@ -0,0 +1,14 @@
+//
+//  Saved.swift
+//  pulse
+//
+//  Created by Adnan Abdulai on 7/14/19.
+//  Copyright © 2019 bdt. All rights reserved.
+//
+
+import UIKit
+
+class Saved {
+    //var savedArticles: [Article]
+
+}
