48f94ff6d775e1432f867265908181b7aa3a5008
emilshouse <emilshouse@gmail.com>
2020-04-04 18:47:10 -0700

    Aa afn 18 (#32)
    * Adding Realm pod
    
    * Completing Realm additions
    
    * adding modified pulse file

--- pulse/pulse/AppDelegate.swift
+++ pulse/pulse/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import RealmSwift
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -14,7 +15,16 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.        
+        
+        print(Realm.Configuration.defaultConfiguration.fileURL)
+        
+        do {
+            let realm = try Realm()
+        } catch {
+            print("Could not initialise new realm, \(error)")
+        }
+        
+        
         return true
     }
 
---
+++ pulse/pulse/SavedArticle.swift
@@ -0,0 +1,23 @@
+//
+//  SavedArticle.swift
+//  pulse
+//
+//  Created by Admin on 3/29/20.
+//  Copyright © 2020 bdt. All rights reserved.
+//
+
+import Foundation
+import RealmSwift
+
+class SavedArticle: Object {
+    
+    //newsAPI deliverables
+    @objc dynamic var author: String?
+    @objc dynamic var title: String = ""
+    @objc dynamic var desc: String = ""
+    @objc dynamic var url: String = ""
+    @objc dynamic var imageURL: String = ""
+    @objc dynamic var publishedAt: String = ""
+    @objc dynamic var content: String = ""
+    var parentContainer = LinkingObjects(fromType: Saved.self, property: "savedArticles")
+}
