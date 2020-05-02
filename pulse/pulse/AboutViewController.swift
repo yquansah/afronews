694a209cbf1a1781c5b1e62a059e9f0215800362
emilshouse <emilshouse@gmail.com>
2020-05-01 19:32:20 -0700

    Adding updates to about view.

---
+++ pulse/pulse/AboutViewController.swift
@@ -0,0 +1,34 @@
+//
+//  AboutViewController.swift
+//  pulse
+//
+//  Created by Adnan Abdulai on 5/1/20.
+//  Copyright © 2020 bdt. All rights reserved.
+//
+
+import UIKit
+
+class AboutViewController: UIViewController {
+
+    @IBAction func feedbackBtnTapped(_ sender: UIButton) {
+        aboutAlert()
+    }
+
+    @IBAction func reviewBtnTapped(_ sender: UIButton) {
+        aboutAlert()
+    }
+
+    func aboutAlert() {
+        let ac = UIAlertController(title: "Not Available", message: "These options are not available in the beta stage.", preferredStyle: .alert)
+
+        ac.addAction(UIAlertAction(title: "Gotcha", style: .default, handler: nil))
+        present(ac, animated: true)
+
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
