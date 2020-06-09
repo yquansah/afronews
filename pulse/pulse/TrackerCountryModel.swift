5cf5303bc78dabec988bd92bc926cd0a3e9a7184
Adnan Emil Abdulai <emilshouse@gmail.com>
2020-06-09 14:44:11 -0700

    Adding Covid-Tracker feature. (#55)
    * Adding Covid-Tracker feature.
    
    * Fixing country stats issue.

---
+++ pulse/pulse/TrackerCountryModel.swift
@@ -0,0 +1,17 @@
+//
+//  TrackerCountryModel.swift
+//  pulse
+//
+//  Created by Adnan Abdulai on 6/7/20.
+//  Copyright © 2020 bdt. All rights reserved.
+//
+
+import Foundation
+
+struct TrackerCountryModel {
+
+    var country: String = ""
+    var confirmed: Int = 0
+    var deaths: Int = 0
+    var population: Int = 0
+}
